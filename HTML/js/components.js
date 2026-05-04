22f435c2d78a4d1ff299b8ca4c0d4e9aadba8fb0
Johnston Abubakar Kweku <143568779+johnston-kweku@users.noreply.github.com>
2026-05-04 07:52:35 +0000

    Progress

---
+++ HTML/js/components.js
@@ -0,0 +1,13 @@
+function loadComponent(id, path) {
+    fetch(path)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading component:', error));
+}
+
+// Load the navbar
+document.addEventListener("DOMContentLoaded", () => {
+    loadComponent('navbar-placeholder', '/HTML/components/navbar.html');
+});
